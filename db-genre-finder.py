c2819567470e2cb106a7b6762b60e67f65fe9508
Kate Bailey <ktbailey@unc.edu>
2023-08-22 15:55:18 -0400

    fixes, updated db

--- db-genre-finder.py
+++ db-genre-finder.py
@@ -5,8 +5,11 @@ import re
 from pathlib import Path
 
 
-YOUR_EMAIL = ""
-DISCOGS_API_TOKEN = ""
+# important notes: the index (i.e. row number in the csv) is NOT the same as the card catalog id number! so don't worry if those don't line up
+# your wifi MUST be stable or else the program will crash
+
+YOUR_EMAIL = "ktbailey@unc.edu"
+DISCOGS_API_TOKEN = "WYGUVmaQhjlcAOqIVTTzImqgtfLxJRNgQSwqooLK"
 NO_DATA = None
 last_index: int = 0
 
@@ -21,14 +24,18 @@ filename = Path("wxyc_db/wxyc_db.csv")
 start_idx = int(input("Enter the starting row index: "))
 end_idx = int(input("Enter the ending row index: "))
 specific_rows = range(start_idx, end_idx+1)
+total_rows = end_idx - start_idx
 
 df = pd.read_csv(filename, skiprows = lambda x: (x not in specific_rows and x != 0))
-df = df.rename(columns={"Genre":"StationGenre"})
 
 # make columns with default value (no data)
 df['DiscogsID'] = NO_DATA
 df['DiscogsURL'] = NO_DATA
 df['Checked'] = NO_DATA
+for i in range(1,6):
+    df[f'DiscogsGenre{i}'] = NO_DATA
+for j in range(1,6):
+    df[f'DiscogsGenre{j}'] = NO_DATA
 
 
 print(f"{len(df)} items in specified portion of wxyc library. starting discogs now...")
@@ -39,7 +46,11 @@ start_time = time.time()
 
 # row by row to get data from discogs api
 for index,rows in df.iterrows():
-    time.sleep(0.6)
+    time.sleep(1)
+    if last_index % (round(total_rows/10)) == 0 and last_index != 0:
+        print(f"{(last_index/round(total_rows/10))*10}% done")
+        output_filename = f"output/wxyc_with_discogs_{start_idx}_to_{end_idx}_BACKUP.csv"
+        df.to_csv(output_filename, index=False)
 
     # some exception handling - will probably eventually move this to a different file to run a second time, on whatever didn't match
     df.at[index, "Checked"] = "yes"
@@ -50,7 +61,7 @@ for index,rows in df.iterrows():
     entry_format = df.at[index,"Format"]
     found_type = "master"
 
-    print(f"starting search for {entry_title}")
+    # print(f"starting search for {entry_title}")
 
     if "[" in entry_title:
         entry_title = re.sub("\[.*?\]","",entry_title)
@@ -62,16 +73,31 @@ for index,rows in df.iterrows():
     if "Various Artists" in entry_artist:
         entry_artist = "Various"
     
-    results = d.search(entry_title,artist=entry_artist,type="master")
-    print(f"{entry_title} has {len(results)} for master")
+    try:
+        results = d.search(entry_title,artist=entry_artist,type="master")
+    except ConnectionError:
+        print(f"Connection error at Release ID {df.at[index, 'ReleaseId']}, saving backup")
+        output_filename = f"output/wxyc_with_discogs_{start_idx}_to_{end_idx}_BACKUP.csv"
+        df.to_csv(output_filename, index=False)
+    # print(f"{entry_title} has {len(results)} for master")
     if len(results) == 0:
-        time.sleep(0.9)
-        results = d.search(entry_title,artist=entry_artist,type="release")
-        print(f"{entry_title} has {len(results)} for release")
+        time.sleep(1)
+        try:
+            results = d.search(entry_title,artist=entry_artist,type="release")
+        except ConnectionError:
+            print(f"Connection error at Release ID {df.at[index, 'ReleaseId']}, saving backup")
+            output_filename = f"output/wxyc_with_discogs_{start_idx}_to_{end_idx}_BACKUP.csv"
+            df.to_csv(output_filename, index=False)
+        # print(f"{entry_title} has {len(results)} for release")
         found_type = "release"
         if len(results) == 0 and entry_artist != "Various" and entry_artist != "":
-            time.sleep(0.9)
-            results = d.search(artist=entry_artist,type="master")
+            time.sleep(1)
+            try:
+                results = d.search(artist=entry_artist,type="master")
+            except:
+                print(f"Connection error at Release ID {df.at[index, 'ReleaseId']}, saving backup")
+                output_filename = f"output/wxyc_with_discogs_{start_idx}_to_{end_idx}_BACKUP.csv"
+                df.to_csv(output_filename, index=False)
             df.at[index, "Checked"] = "yes but artist"
             found_type = "master"
 
@@ -85,11 +111,11 @@ for index,rows in df.iterrows():
     df.at[index, "DiscogsURL"] = f"https://www.discogs.com/{found_type}/{first_result.id}"
     # print(f"we found one for {index}\n")
 
-    print(first_result)
+    # print(first_result)
     try:
         # ok cool so each result is a discogs object
         # we can get genres from it
-        print(first_result.genres)
+        # print(first_result.genres)
         genre = first_result.genres
         for i in range(len(genre)):
             # adds genre to appropriate column
@@ -101,7 +127,7 @@ for index,rows in df.iterrows():
     
     try: 
         # same thing for styles (i.e. sub-genres)
-        print(f"{first_result.styles}\n\n")
+        # print(f"{first_result.styles}\n\n")
         style = first_result.styles
         for j in range(len(style)):
             if j < 5:
